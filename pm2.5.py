f47e757e00c6fb84881668b1d074c37b5adc23de
silverware03 <kyleyoon16@gmail.com>
2025-02-12 15:27:16 -0800

    Update pm2.5.py

--- pm2.5.py
+++ pm2.5.py
@@ -50,8 +50,7 @@ pm_data = open("pm25_data.csv",'w', newline = None)
 csvwriter = csv.writer(pm_data,delimiter = ',')
 csvwriter.writerow(["Time","PM 1.0 Standard Conc.","PM 2.5 Standard Conc.","PM 10 Standard Conc.", "Particles > 0.3um / 0.1L air:",
                     "Particles > 0.5um / 0.1L air:", "Particles > 1.0um / 0.1L air:","Particles > 2.5um / 0.1L air:","Particles > 5.0um / 0.1L air:",
-                    "Particles > 10 um / 0.1L air:"
-])
+                    "Particles > 10 um / 0.1L air:"])
 aqdata=pm25.read()
 for 1 in range(60):
 
