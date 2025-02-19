0a5e8f2c0957f681702e064395cb2ff237a25cfe
silverware03 <kyleyoon16@gmail.com>
2025-02-19 15:06:51 -0800

    Update week5code.py

--- week5code.py
+++ week5code.py
@@ -19,7 +19,7 @@ i2c = board.I2C()
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 bme680.sea_level_pressure = 1013.25
 
-timelimit = 120
+timelimit = 60
 ctime = 0
 
 mobiledata = open("BME680_PM25.csv",'w', newline = None)
@@ -27,7 +27,8 @@ csvwriter = csv.writer(mobiledata,delimiter = ',')
 csvwriter.writerow(["Time","PM 1.0 Standard Conc.","PM 2.5 Standard Conc.","PM 10 Standard Conc.",
                     "Particles > 0.3um / 0.1L air:", "Particles > 0.5um / 0.1L air:",
                     "Particles > 1.0um / 0.1L air:", "Particles > 2.5um / 0.1L air:",
-                    "Particles > 5.0um / 0.1L air:", "Particles > 10 um / 0.1L air:"])
+                    "Particles > 5.0um / 0.1L air:", "Particles > 10 um / 0.1L air:",
+                    "Temperature","Gas","Humidity","Pressure","Altitude"])
 while ctime < timelimit:
     ctime +=1
     aqdata = pm25.read()
@@ -44,15 +45,13 @@ while ctime < timelimit:
     um25 = aqdata["particles 25um"]
     um50 = aqdata["particles 50um"]
     um100 = aqdata["particles 100um"]
-    print("\nTemperature: %0.1f C" % bme680.temperature)
-    print("Gas: %d ohm" % bme680.gas)
-    print("Humidity: %0.1f" % bme680.relative_humidity)
-    print("Pressure: %0.3f hPa" % bme680.pressure)
 
-    print("Altitude = %0.2f meters" % bme680.altitude)
+    temp = round(bme680.temperature,2)
+    gas = round(bme680.gas,2)
+    humid = round(bme680.humidity,2)
+    pressure = round(bme680.pressure,2)
+    altitude = round(bme680.altitude,2)
 
-
-    csvwriter.writerow([timestamp,PM1stan, PM2_5stan, PM10stan,um03,um05,um10,um25,um50,um100])
+    csvwriter.writerow([timestamp,PM1stan, PM2_5stan, PM10stan,um03,um05,um10,um25,um50,um100,temp,gas,humid,pressure,altitude])
     time.sleep(1)
 mobiledata.close()
-print("Done!!")
