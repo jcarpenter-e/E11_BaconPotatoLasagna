fa078d41cc4e19bb68ca785acd97834cbe250a4e
silverware03 <kyleyoon16@gmail.com>
2025-02-19 15:20:44 -0800

    Update week5code.py

--- week5code.py
+++ week5code.py
@@ -7,6 +7,7 @@ from digitalio import DigitalInOut, Direction, Pull
 
 import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+reset_pin = None
 
 from adafruit_pm25.uart import PM25_UART
 pm25 = PM25_UART(uart, reset_pin)
