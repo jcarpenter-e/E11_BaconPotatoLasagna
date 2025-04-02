6143dafdb2e7c725d0de3e1b56865aaeb4781dc7
silverware03 <kyleyoon16@gmail.com>
2025-04-02 14:26:01 -0700

    Update Oscilloscope.py

--- Oscilloscope.py
+++ Oscilloscope.py
@@ -13,12 +13,11 @@ def my_callback(channeln):
     global count 
     count += 1
 
-GPIO.add_event_detect(11, GPIO.FALLING, callback= my_callback)  # add rising edge detection on a channel
-
+GPIO.add_event_detect(11, GPIO.FALLING, callback= my_callback)
 timelimit = int(sys.argv[1])
 counttime = 0
 
-filename = str(sys.argv[2]) + ".csv"
+filename = str(sys.argv[3]) + ".csv"
 mobiledata = open(filename,'w', newline = None)
 csvwriter = csv.writer(mobiledata,delimiter = ',')
 csvwriter.writerow(["Counts in the Last Minute"])
@@ -27,5 +26,5 @@ while counttime < timelimit:
     counttime += 1
     csvwriter.writerow([count])
     count = 0
-    time.sleep(10)
+    time.sleep(int(sys.argv[2]))
 mobiledata.close()
